7da6c13ded8db8711583a0383baff776cde79802
Orma <web23aormaechea@imaltuna.com>
2024-02-28 20:49:07 +0100

    Agregar pestaña contacto

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace BulkyWeb.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
