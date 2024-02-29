cfe377a667cf8aeef10a9b551d74f340f015f3c3
web23aormaechea <web23aormaechea@imaltuna.com>
2024-02-29 11:27:58 +0100

    Agregar pestaña Category y rellenarla con datos

--- BulkyWeb/wwwroot/js/site.js
+++ BulkyWeb/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function botoncito(){
+    alert("Estas en la pagina de contactos")
+}
